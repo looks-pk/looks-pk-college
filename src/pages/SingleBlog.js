4a982249965da9de9d145c9cf52827c260bb4796
abdullah aqeel <abdullahaqeelpakarab@gmail.com>
2023-02-08 12:37:45 +0500

    single blog Commit

--- src/pages/SingleBlog.js
+++ src/pages/SingleBlog.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Breadcrubms from '../components/Breadcrubms'
 import { Helmet } from 'react-helmet'
 import Meta from '../components/Meta'
+import { Link } from 'react-router-dom'
+import { HiOutlineArrowLeft } from 'react-icons/hi'
 
 const SingleBlog = () => {
     return (
@@ -16,9 +18,11 @@ const SingleBlog = () => {
                     <div className="row">
                         <div className="col-12">
                             <div className='single-blog-card'>
+                                <Link className='d-flex align-items-center gap-10 col-2' to={'/blogs'}> <HiOutlineArrowLeft className='fs-3' /> Go Back To Blogs</Link>
                                 <h3 className='title'>A Beautifull Sunday Morning</h3>
                                 <img className='img-fluid w-100 my-4' src="/images/blog-1.jpg" alt="" />
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque fugit nisi amet magnam suscipit laboriosam, provident quod voluptatem ipsum quam illum vero incidunt mollitia nemo commodi deleniti at. Ipsum facere tempora, sunt deserunt facilis delectus sit eius autem voluptate magnam eum at corrupti debitis alias magni iste odit vel deleniti consequuntur. Nisi laborum ipsum eligendi possimus ipsam a, esse, sint illum excepturi nam quae tempore quibusdam quidem magni quod illo eius. Non dolore ullam ducimus facilis magni impedit cupiditate perferendis vero. Facilis natus rem nulla quo vitae modi maiores officiis consequuntur error aliquid. Ipsam reiciendis neque in rerum cupiditate. Iusto?</p>
+                                <p></p>
                             </div>
                         </div>
                     </div>
